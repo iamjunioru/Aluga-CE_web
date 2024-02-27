5deabd7a8d9c635cc69984ad1a71ae5987e8ab0d
Junior <72615418+iamjunioru@users.noreply.github.com>
2024-02-27 19:16:36 -0300

    final

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.covergbfn7"
+    "revision": "0.f47h0u6suo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,39 @@ export default defineConfig({
     registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      manifest: {
+        "background_color": "#ffffff",
+        "dir": "ltr",
+        "start_url": "/",
+        "display": "standalone",
+        "name": "Aluga-CE PWA",
+        "orientation": "any",
+        "scope": "/",
+        "short_name": "Aluga-CE",
+        "theme_color": "#3a9aa5",
+        "icons": [
+          {
+            "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          }
+        ],
+        "screenshots": [
+          {
+            "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/screenshots/1280x800-screenshot.png",
+            "sizes": "1280x800",
+            "type": "image/png"
+          }
+        ],
+        "description": "pwa."
       }
   })],
 })
