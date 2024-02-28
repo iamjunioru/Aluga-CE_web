6764e669c636824361bca82967bf7318e7d21e99
Junior <iamjunioru@gmail.com>
2024-02-28 06:35:47 -0300

    feat: api

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.REACT_APP_BASE_URL || 'http://localhost:8000';
+const baseURL = import.meta.env.REACT_APP_BASE_URL || 'https://aluga-ce-api.vercel.app';
 
 const http = axios.create({ 
   baseURL,
