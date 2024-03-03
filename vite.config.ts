faf843497429e0a92cda355d167b2396568df102
Junior <iamjunioru@gmail.com>
2024-03-03 16:28:55 -0300

    fix: icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       devOptions: {
-        enabled: true
+        enabled: true,
       },
       manifest: {
-        "name": "AlugaCE",
-        "short_name": "AlugaCE",
-        "start_url": "/",
-        "display": "standalone",
-        "background_color": "#ffffff",
-        "lang": "pt",
-        "scope": "/",
-        "dir": "ltr",
-        "orientation": "natural",
-        "theme_color": "#0096c7",
-          "icons": [
-            {
-              "src": "assets/android/android-launchericon-512-512.png",
-              "sizes": "512x512"
-            },
-            {
-              "src": "assets/android/android-launchericon-192-192.png",
-              "sizes": "192x192"
-            },
-            {
-              "src": "assets/android/android-launchericon-144-144.png",
-              "sizes": "144x144"
-            },
-            {
-              "src": "assets/android/android-launchericon-96-96.png",
-              "sizes": "96x96"
-            },
-            {
-              "src": "assets/android/android-launchericon-72-72.png",
-              "sizes": "72x72"
-            },
-            {
-              "src": "assets/android/android-launchericon-48-48.png",
-              "sizes": "48x48"
-            }
-          ],
-        "description": "O Aluga-CE é um aplicativo de locação de imóveis no interior do Ceará, oferecendo uma ampla variedade de opções. Com uma interface intuitiva e recursos de busca avançados, os usuários podem encontrar facilmente o imóvel ideal para suas necessidades."
-      }
-    })
+        name: "AlugaCE",
+        short_name: "AlugaCE",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        lang: "pt",
+        scope: "/",
+        dir: "ltr",
+        orientation: "natural",
+        theme_color: "#0096c7",
+        icons: [
+          {
+            src: "assets/android/android-launchericon-512-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "assets/android/android-launchericon-192-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/android/android-launchericon-144-144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+        ],
+        description:
+          "O Aluga-CE é um aplicativo de locação de imóveis no interior do Ceará, oferecendo uma ampla variedade de opções. Com uma interface intuitiva e recursos de busca avançados, os usuários podem encontrar facilmente o imóvel ideal para suas necessidades.",
+      },
+    }),
   ],
-})
+});
