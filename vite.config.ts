6de38ce264cb6cef31bc01d8cdb24f516b586950
Junior <72615418+iamjunioru@users.noreply.github.com>
2024-03-03 04:33:24 -0300

    style: manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -10,38 +10,39 @@ export default defineConfig({
         enabled: true
       },
       manifest: {
-          "name": "Aluga-CE",
-          "short_name": "Aluga-CE",
-          "start_url": "/",
-          "display": "fullscreen",
-          "background_color": "#ffffff",
-          "lang": "pt",
-          "scope": "/",
-          "dir": "ltr",
-          "orientation": "any",
-          "theme_color": "#579fd8",
-          "icons": [
-            {
-              "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/manifest-icon-512.maskable.png",
-              "sizes": "512x512",
-              "type": "image/png",
-              "purpose": "maskable"
-            },
-            {
-              "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/manifest-icon-192.maskable.png",
-              "sizes": "192x192",
-              "type": "image/png",
-              "purpose": "any"
-            }
-          ],
-          "screenshots": [
-            {
-              "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/screenshots/1280x800-screenshot.png",
-              "sizes": "1280x800",
-              "type": "image/png"
-            }
-          ],
-          "description": "pwa."
-        }
+          {
+  "name": "AlugaCE",
+  "short_name": "AlugaCE",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "lang": "pt",
+  "scope": "/",
+  "dir": "ltr",
+  "orientation": "natural",
+  "theme_color": "#0096c7",
+  "icons": [
+    {
+      "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/manifest-icon-512.maskable.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    },
+    {
+      "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/manifest-icon-192.maskable.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any"
+    }
+  ],
+  "screenshots": [
+    {
+      "src": "https://raw.githubusercontent.com/iamjunioru/Aluga-CE_web/main/screenshots/1280x800-screenshot.png",
+      "sizes": "1280x800",
+      "type": "image/png"
+    }
+  ],
+  "description": "O Aluga-CE é um aplicativo de locação de imóveis no interior do Ceará, oferecendo uma ampla variedade de opções. Com uma interface intuitiva e recursos de busca avançados, os usuários podem encontrar facilmente o imóvel ideal para suas necessidades."
+      }
   })],
 })
