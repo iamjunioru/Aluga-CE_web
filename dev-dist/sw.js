f5ebf90393bfe5963c4c5cb881df0cd284cf64f1
Junior <iamjunioru@gmail.com>
2024-03-03 16:44:56 -0300

    fix: update icon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.od34e0bt08g"
+    "revision": "0.e180jgpu2io"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
